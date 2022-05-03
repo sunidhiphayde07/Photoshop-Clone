3febded3fee41f0b0ce56d698743b3c540f41e9b
sunidhiphayde07 <92315437+sunidhiphayde07@users.noreply.github.com>
2022-05-03 12:52:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,126 @@
+import React, {useState} from 'react'
+import './App.css'
+import Slider from './components/Slider'
+import SidebarItem from './components/SidebarItem'
+
+const DEFAULT_OPTIONS =[
+  {
+    name:'Brightness',
+    property:'brightness',
+    value:100,
+    range: {
+      min:0,
+      max:200
+    },
+    unit:'%'
+  },
+  {
+    name:'Contrast',
+    property:'contrast',
+    value:100,
+    range: {
+      min:0,
+      max:200
+    },
+    unit:'%'
+  },
+  {
+    name:'Saturation',
+    property:'saturate',
+    value:100,
+    range: {
+      min:0,
+      max:200
+    },
+    unit:'%'
+  },
+  {
+    name:'Grayscale',
+    property:'grayscale',
+    value:100,
+    range: {
+      min:0,
+      max:100
+    },
+    unit:'%'
+  },
+  {
+    name:'Sepia',
+    property:'sepia',
+    value:100,
+    range: {
+      min:0,
+      max:100
+    },
+    unit:'%'
+  },
+  {
+    name:'Hue Rotate',
+    property:'hue-rotate',
+    value:0,
+    range: {
+      min:0,
+      max:360
+    },
+    unit:'deg'
+  },
+  {
+    name:'Blur',
+    property:'blur',
+    value:0,
+    range: {
+      min:0,
+      max:20
+    },
+    unit:'px'
+  } 
+]
+
+function App() {
+  
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+  const [selectedIndex, setSelectedIndex]=useState(0)
+  const selectedOption = options[selectedIndex]
+
+  function handleSliderChange(e){
+    setOptions(prevOptions => {
+      return prevOptions.map((option, index) => {
+        if(index!== selectedIndex) return option
+        else return {...option, value: e.target.value}
+      })
+    })
+  }
+
+  function getImageStyle(){
+    const filters = options.map(option => {
+      return `${option.property}(${option.value}${option.unit})`
+    })
+    return {filter: filters.join(' ') }
+  }
+
+  return (
+    <div className="container">
+      <div className="main-image" style={getImageStyle()}/>
+         <div className="sidebar">
+             {options.map((option, index)=>{
+               return (
+                 <SidebarItem 
+                 key={index} 
+                 name={option.name} 
+                 active={index ===selectedIndex} 
+                 handleClick={()=> setSelectedIndex(index )}
+                 />
+               )
+             })}
+         </div>
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleSliderChange}
+      />
+    </div>
+  )
+}
+
+export default App;
